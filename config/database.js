7d6ec3fdb1c06cf54b5d2dafe1e4bb28751ce593
David Carvalho <david.snt.carvalho@gmail.com>
2016-11-03 16:11:44 -0200

    Configuração inicial do Mongoose

---
+++ config/database.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+module.exports = function(uri){
+	mongoose.connect(uri);
+
+	mongoose.connection.on('connected', function(){
+		console.log('Mongoose conectado');
+	});
+
+	mongoose.connection.on('disconnected', function(){
+		console.log('Mongoose desconectado');
+	});
+
+	mongoose.connection.on('error', function(){
+		console.error('Mongoose: falha na conexão');
+	});
+
+	process.on('SIGINT', function(){
+		mongoose.connection.close();
+		console.log('Mongoose encerrado');
+		process.exit(0);
+	});
+}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
+var settings = require('./config/settings');
+require('./config/database')(settings.databasePath);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Aplicação rodando na porta ' + app.get('port'));
