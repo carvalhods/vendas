d7b90ac71abba6628e7bebd45e1ad6592473acbc
David Carvalho <david.snt.carvalho@gmail.com>
2016-11-10 20:59:50 -0200

    angular-smart-table instalado

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
-angular.module('vendas', ['ngRoute','ngResource'])
+angular.module('vendas', ['ngRoute', 'ngResource', 'smart-table'])
 	.config(function($routeProvider){
 
 		$routeProvider.when("/produtos", {
 			templateUrl: "partials/produtos.html",
 			controller: "ProdutosController"
 		});
-		
+
 		$routeProvider.otherwise({redirectTo: "/"});
 	})
