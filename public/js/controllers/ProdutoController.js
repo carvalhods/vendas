d0dfbea331aa286d71d7cb87f5caf2e8bd15ebe8
David Carvalho <david.snt.carvalho@gmail.com>
2016-11-16 12:34:39 -0200

    maskmoney e dropdown

--- public/js/controllers/ProdutoController.js
+++ public/js/controllers/ProdutoController.js
@@ -5,6 +5,14 @@ angular.module("vendas").controller("ProdutoController",
             $scope.produto.descricao = $scope.produto.descricao.toUpperCase();
         }
 
-        
+
+        $scope.produto = {
+            codigo: 1001,
+            descricao: 'Produto',
+            qtde: 99,
+            estoqueMin: 10,
+            unidade: 'UN',
+            valor: 1099.9
+        }        
     }
 )
