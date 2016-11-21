29beda27421f308ec155efad8d486d001c14ef91
David Carvalho <david.snt.carvalho@gmail.com>
2016-11-21 16:34:51 -0200

    Focus descricao

--- public/js/controllers/ProdutoController.js
+++ public/js/controllers/ProdutoController.js
@@ -115,3 +115,16 @@ angular.module("vendas").controller("ProdutoController",
         }
     };
 })
+.directive('focusMe', function($timeout) {
+  return {
+    scope: { trigger: '=focusMe' },
+    link: function(scope, element) {
+      scope.$watch('trigger', function(value) {
+        if(value === true) { 
+            element[0].focus();
+            scope.trigger = false;          
+        }
+      });
+    }
+  };
+})
