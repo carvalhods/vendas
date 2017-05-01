be46b0111dc18397c16774be3b6e4cd5a78b0d8f
David Carvalho <david.snt.carvalho@gmail.com>
2017-05-01 18:00:59 -0300

    model Venda

--- app/models/Venda.js
+++ app/models/Venda.js
@@ -1,9 +1,28 @@
-var mongoose = require('mongoose'),
-    autoIncrement = require('mongoose-auto-increment');
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
 mongoose.Promise = global.Promise;
 
 module.exports = function(app){
-    var Schema = mongoose.Schema({
+    var itensSchema = mongoose.Schema({
+      produto: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Produto",
+          unique: true,
+          required: true
+      },
+      qtde: {
+          type: Number,
+          required: [true, "Informe a qtde. de itens de cada produto"],
+          min: [0.01, "A qtde. de itens de cada produto deve ser maior que zero"]
+      },
+      valorUnit: {
+          type: Number,
+          required: [true, "É necessário informar o valor unitário de cada item"],
+          min: [0.01, 'O valor unitário do produto deve ser maior que zero']
+      }
+    });
+
+    var vendaSchema = mongoose.Schema({
         numero: {
           type: Number,
           required: true,
@@ -14,55 +33,30 @@ module.exports = function(app){
           required: [true, "Informe a data da venda"],
           default: Date.now
         },
-        itens: [
-            {
-                produto: {
-                    type: mongoose.Schema.ObjectId,
-                    ref: "Produto"
-                },
-                qtde: {
-                    type: Number,
-                    required: [true, "Informe a qtde. de itens de cada produto"],
-                    min: [0.01, "A qtde. de itens de cada produto deve ser maior que zero"]
-                },
-                valorUnit: {
-                    type: Number,
-                    required: [true, "É necessário informar o valor unitário de cada item"],
-                    min: [0.01, 'O valor unitário do produto deve ser maior que zero']
-                }
-            }
-        ]
+        itens: [itensSchema]
     });
 
-    // Schema.pre('save', function(next) {
-    //   var Produto = app.models.Produto;
-    //   var venda = this;
-    //   var produtos = [];
-    //   var result = [];
-    //
-    //   for (var i in venda.itens) {
-    //     produtos.push(venda.itens[i].produto);
-    //   }
-    //   Produto.find({_id: {$in: produtos} })
-    //   .exec()
-    //   .then(
-    //     function(produtos) {
-    //       for (var i in produtos) {
-    //         if (produtos.length > 0) {
-    //           if (produtos[i].qtde < venda.itens[i].qtde) {
-    //             result.push("A qtde. do produto '" + produtos[i].descricao +
-    //             "' a ser vendida é maior que a qtde. disponível em estoque (" +
-    //             produtos[i].qtde + " " + produtos[i].unidade + ")");
-    //           }
-    //         } else {
-    //           result.push("Produto do item " + (i+1) + " não está cadastrado");
-    //         }
-    //       }
-    //     }
-    //   )
-    //
-    //
-    // })
+    itensSchema.pre('save', function(next) {
+      var Produto = app.models.Produto;
+      var item = this;
+      Produto.findOne({_id: item.produto})
+      .exec()
+      .then(
+        function(produto) {
+          if (produto) {
+            if (produto.qtde < item.qtde) {
+              return next(new Error("A qtde. do produto '" + produto.descricao +
+                  "' a ser vendida é maior que a qtde. disponível em estoque (" +
+                  produto.qtde + " " + produto.unidade + ")"));
+            } else {
+              next();
+            }
+          } else {
+            return next(new Error("Um produto não está cadastrado"));
+          }
+        }
+      )
+    });
 
     autoIncrement.initialize(mongoose.connection);
     Schema.plugin(autoIncrement.plugin, {
@@ -72,5 +66,5 @@ module.exports = function(app){
         incrementBy: 1
     });
 
-    return mongoose.model('Venda', Schema);
+    return mongoose.model('Venda', vendaSchema);
 }
