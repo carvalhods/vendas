abbb38120b7afaa962e60b2fe7937dbd40c451b7
David Carvalho <david.snt.carvalho@gmail.com>
2017-05-15 17:33:26 -0300

    rota da index.html

--- server/config/express.js
+++ server/config/express.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
+var path = require('path');
 var settings = require('./settings');
 
 module.exports = function(){
 	var app = express();
 	app.set('port', settings.port);
-	app.use(express.static('./public'));
-	app.use(express.static('./node_modules/ng-table/bundles'));
-	app.set('view engine', 'ejs');
-	app.set('views', './app/views');
+	app.use(express.static(path.join(__dirname + './../../dist')));
 
 	app.use(require('compression')());
 	app.use(bodyParser.urlencoded({extended:true}));
 	app.use(bodyParser.json());
 	app.use(require('method-override')());
 
-	load('models', {cwd: 'app'})
+	load('models', {cwd: 'server'})
 	.then('controllers')
 	.then('routes')
 	.into(app);
