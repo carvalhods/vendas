3c41ef90a05b9a178fd0328d8ebbe54a560a17e4
David Carvalho <david.snt.carvalho@gmail.com>
2017-05-16 17:02:07 -0300

    correção semantic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NgSemanticModule } from 'ng-semantic';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +14,6 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgSemanticModule,
     AppRoutingModule
   ],
   providers: [],
