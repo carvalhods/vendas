6cf9256663bd7a51842b5c431c9e346ad8646b83
David Carvalho <david.snt.carvalho@gmail.com>
2017-06-02 14:23:36 -0300

    semantic-ui-calendar install

--- src/app/vendas-lancamento/vendas-lancamento.component.ts
+++ src/app/vendas-lancamento/vendas-lancamento.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendasLancamentoComponent implements OnInit {
 
+  status: any = {saved: false, msg: null, erros: []};
+
   constructor() { }
 
   ngOnInit() {
